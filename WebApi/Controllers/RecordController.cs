873cf5fd23237a7ab4f622897a361422c8122829
andre <andreyshe22@gmail.com>
2023-07-10 16:43:52 +0500

    изменен endpoint записи на инд. событие

--- WebApi/Controllers/RecordController.cs
+++ WebApi/Controllers/RecordController.cs
@@ -1,6 +1,43 @@
-﻿namespace WebApi.Controllers
+﻿using Application.Records.IndividualRecords.Commands.Create;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace WebApi.Controllers
 {
-    public class RecordController
+    [ApiController]
+    [Route("/api/records")]
+    public class RecordController : ControllerBase
     {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public RecordController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Записаться на индивидуальное событие
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPost("record{id}")]
+        [Authorize]
+        public async Task<ActionResult> CreateRecord(Guid id)
+        {
+            var command = new CreateIndividualRecordCommand
+            {
+                ClientId = Guid.Parse(User.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value),
+                ReservationId = id
+            };
+
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
     }
 }
